eba6802dbbd12c8de4e0f7397d79d532ff208af0
unknown <nitinpatil653@gmail.com>
2022-05-01 23:06:38 +0530

    Changes made

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+	
+	public static void main(String arg[]) {
+		
+		int a=12;
+		int b=15;
+		
+	if (a>b) {
+		System.out.println("Test Case Failed");
+		
+	}
+	else {
+		System.out.println("Test case passed");
+	}
+	}
+}
