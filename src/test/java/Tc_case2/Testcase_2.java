b266ef4b016d3e3bb0d1349d617f3cf8066bb638
unknown <nitinpatil653@gmail.com>
2022-05-05 17:02:04 +0530

    New Code 2

---
+++ src/test/java/Tc_case2/Testcase_2.java
@@ -0,0 +1,22 @@
+package Tc_case2;
+
+public class Testcase_2 {
+	
+		
+		public static void main(String arg[]) {
+			
+			int a=12;
+			int b=15;
+			
+		if (a>b) {
+			System.out.println("Test Case Failed");
+			
+		}
+		else {
+			System.out.println("Nitin patil");
+			System.out.println("GIT success");
+		}
+		}
+	}
+
+
